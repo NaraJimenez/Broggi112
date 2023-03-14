9429b55b811dcab61dc43909907daa5174fcec7b
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-14 14:01:20 +0100

    IconUser

--- resources/views/plantilla/principal.blade.php
+++ resources/views/plantilla/principal.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         
-        <script defer src="https://use.fontawesome.com/releases/v6.0.0/js/all.js"
-        integrity="sha384-l+HksIGR+lyuyBo1+1zCBSRt6v4yklWu7RbG0Cv+jDLDD9WFcEIwZLHioVB4Wkau" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
         <!--El favicon-->
         <link rel="icon" href="{{ asset("img/IconoLogo.png") }}">
@@ -22,8 +22,18 @@
     <body id="body">
         <!--Meter aqui el navbar-->
         <!--Apartado del user-->
-        <div>Funciona</div>
+        <nav class="navbar fixed-top navbar-light">
+            <div class="collapse navbar-collapse navbar-left"  id="navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                   <li class="active"><a href="#">Home</a></li>
+                   <li><a href="#">About</a></li>
+                   <li><a href="#">Contact</a></li>
+                </ul>
+             </div>
+          </nav>
         <!--Footer-->
+        
+
 
     </body>
 
