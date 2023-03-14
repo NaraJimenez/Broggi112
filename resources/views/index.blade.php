078b48a6289aaf270e1d847bb2d7cecce0d8bbe0
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-14 13:45:54 +0100

    favicon + algo de paginas

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+
+@extends('plantilla.principal')
+
+<!--Icono del logo - Falta por Poner-->
+@section('logo_title')
+    <link rel="icon" href="{{ asset('img/IconoLogo.png') }}" type="image/png">
+@endsection
+
+@section('title')
+    Broggi112
+@endsection
+
+@section('css')
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+@endsection
---
+++ resources/views/plantilla/principal.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <script defer src="https://use.fontawesome.com/releases/v6.0.0/js/all.js"
+        integrity="sha384-l+HksIGR+lyuyBo1+1zCBSRt6v4yklWu7RbG0Cv+jDLDD9WFcEIwZLHioVB4Wkau" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <!--El favicon-->
+        <link rel="icon" href="{{ asset("img/IconoLogo.png") }}">
+        <title>@yield("title")</title>
+
+        <!--CSS-->
+        <link rel="stylesheet" href="{{ asset("css/index.css") }}">
+
+    </head>
+
+    <body id="body">
+        <!--Meter aqui el navbar-->
+        <!--Apartado del user-->
+        <div>Funciona</div>
+        <!--Footer-->
+
+    </body>
+
+</html>
