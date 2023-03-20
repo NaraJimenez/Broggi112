e268b88a22759af5cc1ee82cf756808c76ff1ce4
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-20 13:54:41 +0100

    Carta1

--- resources/views/paginas/carta1.blade.php
+++ resources/views/paginas/carta1.blade.php
@@ -11,14 +11,58 @@
             <h2>Carta de Trucada</h2>
           </div>
 
-          <div class="card" id="CartaIncidencia">
+          <div class="card mt-3 mx-auto" id="CartaIncidencia">
             <div class="card-body">
-              This is some text within a card body.
+
+                <!--BOTONES OPCIONES DE RELLENO-->
+              <div id="botonesConjunto" class="mt-3 mb-1 d-flex justify-content-center">
+                <div id="boton1">
+                  <div id="div" class="text-center mt-2">1<br>Incidencias</div>
+                </div>
+                <div id="boton2">
+                  <div id="div" class="text-center mt-2">2<br>Localización</div>
+                </div>
+                <div id="boton3" >
+                  <div id="div" class="text-center mt-2">3<br>Agencias</div>
+                </div>
+              </div>
+
+            <div>
+              <!--PRIMER RECTANGULO:-->
+              <div id="primerRectangulo" class="mt-4 ms-4">
+                <div id="contenidoPrimerRectangulo" class="mt-2">
+                  <div id="numTelf" class="mt-4 ms-4 text-muted">Núm. Telf</div>
+                  <div id="codigoLlamada" class="mt-4 text-muted">Código Llamada</div>
+                  <div id="tiempoLlamada" class="text-muted text-center"> TIEMPO LLAMADA</div>
+                  <div id="notaComuna" class="text-muted">Nota Comuna</div>
+                </div>
+              </div>
+
+             
+
+              <!--SEGUNDO RECTANGULO-->
+              <div id="segundoRectangulo" class="mt-4">
+                <select id="tipoInci" class="form-select form-select-sm" aria-label=".form-select-lg example">
+                  <option selected>Open this select menu</option>
+                  <option value="1">One</option>
+                </select>
+                
+                <select id="incidenciaInci" class="form-select form-select-sm"  aria-label=".form-select-sm example">
+                  <option selected>Open this select menu</option>
+                  <option value="1">One</option>
+                </select><!--
+              <div id="definicionInci" class="text-muted text-center"> TIEMPO LLAMADA</div>
+              <div id="indicacionesInci" class="text-muted">Nota Comuna</div>-->
+            </div> 
+              
+
+              <!--Buscador de Expediente-->
+              <div id="buscadorExpe"></div>
+            </div>
+
+              
             </div>
           </div>
 
 
         
-
-
-<
