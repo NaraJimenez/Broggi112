f57e99685b12ec046f61c221897d76a879b7cb30
Laia Barquero <barquero.b.laia@gmail.com>
2023-03-24 10:16:38 +0100

    -Wip

---
+++ resources/components/atoms/NavbarButton/NavBarButton.vue
@@ -0,0 +1,27 @@
+
+<template>
+<button>
+    <image src= {{icon}} alt=''>
+    <p>{{text}}</p>
+</button>
+</template>
+
+<script>
+import Vue from 'vue';
+    export default{
+        name: "NavbarButton",
+        props: {
+            icon: String,
+            text: String
+        },
+        data() {
+            return {
+                icon: '',
+                text: ''
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
