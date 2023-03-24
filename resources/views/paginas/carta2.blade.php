a496b30c60ed5efe892c7c13736af57b14cec928
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-24 13:45:45 +0100

    Retocada Carta 2

--- resources/views/paginas/carta2.blade.php
+++ resources/views/paginas/carta2.blade.php
@@ -62,21 +62,29 @@
               <div id="segundoRectangulo2" class="mt-4">
 
                 <!--Botones opciones localización-->
-                <div id="botonesLocalización">
+                <div id="botonesLocalizacion" class=" d-flex justify-content-center">
                   <div id="botonLoc1">
-
+                    <a href="#" id="botonCarta2">
+                      <div id="div" class="text-center mt-2"><b>Carretera</b></div>
+                    </a>
                   </div>
 
                   <div id="botonLoc2">
-
+                    <a href="#" id="botonCarta2">
+                      <div id="div" class="text-center mt-2"><b>Calle</b></div>
+                    </a>
                   </div>
 
                   <div id="botonLoc3">
-
+                    <a href="#" id="botonCarta2">
+                      <div id="div" class="text-center mt-2"><b> Punto Singular</b></div>
+                    </a>
                   </div>
 
                   <div id="botonLoc4">
-
+                    <a href="#" id="botonCarta2">
+                      <div id="div" class="text-center mt-2"><b> Población</b></div>
+                    </a>
                   </div>
                 </div>
 
