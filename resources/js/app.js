6bad556c1ecc08ff42c7b633fbdbd3c18f9d24b3
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-27 13:55:34 +0200

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap'
+import { createApp } from 'vue'
+import cartaTrucada1 from './components/cartaTrucada1.vue'
+
+//Vue comnatdo en una etiqueta llamada carta1
+createApp(cartaTrucada1).mount('#carta1');
