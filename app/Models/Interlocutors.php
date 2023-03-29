4bbdddd9aa6711b694d1592b50fa43d60ec658ed
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-29 13:27:17 +0200

    Interlocutors de la BD

--- app/Models/Interlocutors.php
+++ app/Models/Interlocutors.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Interlocutors extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * Get all of the Cartes_trucades for the Interlocutors
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cartes_trucades(): HasMany
+    {
+        return $this->hasMany(Cartes_trucades::class, 'interlocutros_id');
+    }
+
+    /**
+     * Get all of the Cartes_trucades (trucada) for the Interlocutors
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cartes_trucades_trucada(): HasMany
+    {
+        return $this->hasMany(Cartes_trucades::class, 'interlocutros_id_trucada');
+    }
 }
