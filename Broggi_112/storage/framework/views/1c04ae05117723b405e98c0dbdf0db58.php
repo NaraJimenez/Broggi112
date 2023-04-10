f945e5333a0b445891a7e1d2806da21e6a9ce889
OEspejol <100763991+OEspejol@users.noreply.github.com>
2023-04-10 09:44:27 +0200

    Adminusers
    Lo subo en zip que no se porque se me buggea al cp el cg

--- Broggi_112/storage/framework/views/1c04ae05117723b405e98c0dbdf0db58.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('contenido'); ?>
-    {
-    
-    { $user->rol->nombre}
-    }
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.principal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\cep.DESKTOP-56UD49B\Desktop\xampp\htdocs\oriol\resources\views/partials/home.blade.php ENDPATH**/ ?>
