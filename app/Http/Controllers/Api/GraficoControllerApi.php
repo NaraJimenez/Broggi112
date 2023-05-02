e45f2b5179ebc891010aa26e610b00480138e045
Desi Diaz <ddiazt2122@politecnics.barcelona>
2023-05-02 10:02:35 +0200

    Cambio

--- app/Http/Controllers/Api/GraficoControllerApi.php
+++ app/Http/Controllers/Api/GraficoControllerApi.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Clases\Utilitat;
 use App\Models\Expedients;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\Api\CartesTrucadesController;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Database\QueryException;
